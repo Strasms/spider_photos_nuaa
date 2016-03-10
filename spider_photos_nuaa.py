d1ab0dfd925d84d8628f214cf9a9c0ae74b14b44
Root-lee <54rootlee@gmail.com>
2016-03-10 23:50:09 +0200

    爬取所有学院、年级的学生信息
    提高了程序稳定性  减少崩溃概率

--- spider_photos_nuaa.py
+++ spider_photos_nuaa.py
@@ -13,12 +13,12 @@ headers = {'Host':'ded.nuaa.edu.cn',
            'Referer':'http://ded.nuaa.edu.cn/JwSys/Manager/Messages.aspx?OPID=01706',
            'Cookie' :'mycookie'}
 
-colleges = ['15']
-years = ['12']
-specials = ['2']
-num_classes = ['02']
+colleges = ['08']   #学院
+years = ['12']      #年级
+specials = ['1','2','3','4','5','6','7','8','9']   #专业
+num_classes = ['01','02','03','04','05','06','07']    #班级
 
-def num2str(num):
+def num2str(num):          #将数字转换为字符串 如 1 转换为 '01'
     if ((num>0) & (num<10)):
         return '0'+str(num)
     else:
@@ -29,19 +29,28 @@ for college in colleges:
         for special in specials:
             for num_class in num_classes:
                 for people in range(1,40):
-                    student = college + year + special + num_class + num2str(people)
-                    r = requests.get('http://ded.nuaa.edu.cn/JwSys/Manager/Module/EASys/Controls/ImageHandler.ashx?ImageName=%s&BinaryType=xh' % student,headers=headers,timeout=20)   #GET命令获取图片
-                    picpath = college+'\\'+year+'\\'+special+num_class
+                    while True:
+                        student = college + year + special + num_class + num2str(people)   #学号组成方法
+                        pic_url='http://ded.nuaa.edu.cn/JwSys/Manager/Module/EASys/Controls/ImageHandler.ashx?ImageName=%s&BinaryType=xh' % student
+                        try:
+                            r = requests.get(pic_url,headers=headers,timeout=20)   #GET命令获取图片
+                        except:
+                            break
+                        picpath = college+'\\'+year+'\\'+special+num_class   #图片保存路径
+                        if r.url==pic_url: 
+                            break      #用响应url来验证Cookie有效性
+                        else:
+                            print 'Cookie is not effective,Please input another Cookie:'
+                            headers['Cookie']=raw_input()
                     try:
                     #Image.open(StringIO(r.content)).show()    # 显示图片
                         try:
-                            print student
                             Image.open(StringIO(r.content))
                             if not os.path.exists(picpath):
                                 os.makedirs(picpath)
                         except:
-                            print 'aa'
+                            print 'no class!'
                         Image.open(StringIO(r.content)).save('%s/%s.jpg' % (picpath,student))   #保存图片
                         print student+'succesful!'
                     except:
-                        print 'error'
+                        print student+'failed!'

fad4d4cb76deacb59b80c813910d9b526ffabd85
Root-lee <54rootlee@gmail.com>
2016-03-10 19:45:20 +0200

    可以获取某一个班级全部人信息
    可以获取某一个班级全部人信息, 且按年级学院班级建立文件夹, 把个人信息保存在本地

--- spider_photos_nuaa.py
+++ spider_photos_nuaa.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-  
 import requests
-import urllib
 from PIL import Image
 from StringIO import StringIO
+import os
 
 #以下为http通信协议的头，cookie为身份认证信息
 headers = {'Host':'ded.nuaa.edu.cn',   
@@ -13,7 +13,35 @@ headers = {'Host':'ded.nuaa.edu.cn',
            'Referer':'http://ded.nuaa.edu.cn/JwSys/Manager/Messages.aspx?OPID=01706',
            'Cookie' :'mycookie'}
 
-r = requests.get('http://ded.nuaa.edu.cn/JwSys/Manager/Module/EASys/Controls/ImageHandler.ashx?ImageName=151210101&BinaryType=xh',headers=headers,timeout=10)   #GET命令获取图片
+colleges = ['15']
+years = ['12']
+specials = ['2']
+num_classes = ['02']
 
-Image.open(StringIO(r.content)).show()    # 显示图片
-Image.open(StringIO(r.content)).save('me.jpg')   #保存图片
+def num2str(num):
+    if ((num>0) & (num<10)):
+        return '0'+str(num)
+    else:
+        return str(num)
+
+for college in colleges:
+    for year in years:
+        for special in specials:
+            for num_class in num_classes:
+                for people in range(1,40):
+                    student = college + year + special + num_class + num2str(people)
+                    r = requests.get('http://ded.nuaa.edu.cn/JwSys/Manager/Module/EASys/Controls/ImageHandler.ashx?ImageName=%s&BinaryType=xh' % student,headers=headers,timeout=20)   #GET命令获取图片
+                    picpath = college+'\\'+year+'\\'+special+num_class
+                    try:
+                    #Image.open(StringIO(r.content)).show()    # 显示图片
+                        try:
+                            print student
+                            Image.open(StringIO(r.content))
+                            if not os.path.exists(picpath):
+                                os.makedirs(picpath)
+                        except:
+                            print 'aa'
+                        Image.open(StringIO(r.content)).save('%s/%s.jpg' % (picpath,student))   #保存图片
+                        print student+'succesful!'
+                    except:
+                        print 'error'

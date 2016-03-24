d139990b26bf3d114eaecf1c4afdb6a64a67e757
Root-lee <54rootlee@gmail.com>
2016-03-24 17:27:58 +0200

    修复BUG，增加无效班级判断
    修复BUG，该BUG导致每次get请求都加上初始的headers导致Cookie依然会失效。增加了判断班级存在与否语句：如果连续三次无法得到学生信息,则程序默认后面没有学生信息了,开始下一个班级遍历,此做法可以保证程序时间不浪费在无用数据上。

--- spider_photos_nuaa.py
+++ spider_photos_nuaa.py
@@ -13,7 +13,10 @@ headers = {'Host':'ded.nuaa.edu.cn',
            'Referer':'http://ded.nuaa.edu.cn/JwSys/Manager/Messages.aspx?OPID=01706',
            'Cookie' :'mycookie'}
 
-colleges = ['12']   #学院
+s=requests.Session()  #创建会话，可以保持cookie
+s.headers.update(headers)  #默认headers参数
+
+colleges = ['07','10','16']   #学院
 years = ['12']      #年级
 specials = ['1','2','3','4','5','6','7','8','9']   #专业
 num_classes = ['01','02','03','04','05','06','07']    #班级
@@ -28,27 +31,26 @@ for college in colleges:
     for year in years:
         for special in specials:
             for num_class in num_classes:
+                error_count=0
                 for people in range(1,40):
-                    while True:
-                        student = college + year + special + num_class + num2str(people)   #学号组成方法
-                        pic_url='http://ded.nuaa.edu.cn/JwSys/Manager/Module/EASys/Controls/ImageHandler.ashx?ImageName=%s&BinaryType=xh' % student
+                    if error_count==3:
+                        break
+                    student = college + year + special + num_class + num2str(people)   #学号组成方法
+                    picpath = college+'\\'+year+'\\'+special+num_class   #图片保存路径
+                    pic_url='http://ded.nuaa.edu.cn/JwSys/Manager/Module/EASys/Controls/ImageHandler.ashx?ImageName=%s&BinaryType=xh' % student
+                    while True:    #一直到成功发出get请求为止
                         try:
-                            r = requests.Session().get(pic_url,headers=headers,timeout=40)   #GET命令获取图片
-                        except:
+                            r = s.get(pic_url,timeout=40)   #GET命令获取图片
                             break
-                        picpath = college+'\\'+year+'\\'+special+num_class   #图片保存路径
-                        if r.url==pic_url: 
-                            break      #用响应url来验证Cookie有效性
-                        else:
-                            print 'Cookie is not effective,Please input another Cookie:'
-                            headers['Cookie']=raw_input()
+                        except:
+                            print "timeout! Try again..."
                     try:
-                    #Image.open(StringIO(r.content)).show()    # 显示图片
                         try:
                             Image.open(StringIO(r.content))
                             if not os.path.exists(picpath):
                                 os.makedirs(picpath)
                         except:
+                            error_count=error_count+1
                             print 'no class!'
                         Image.open(StringIO(r.content)).save('%s/%s.jpg' % (picpath,student))   #保存图片
                         print student+'succesful!'
